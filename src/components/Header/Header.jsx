c289ec38a90d17bd3f29c01e623f58e4fb891291
Oleg Gordienov <ogordienov@yandex.ru>
2022-08-11 19:54:01 +0200

    feat: add media queries for pads for main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,20 @@ function Header() {
   return (
     <header className='header'>
       <img className='header__logo' alt='Логотип: зеленая снежинка' src={logo} />
-      <div className='header__auth'>
-        <NavLink to='/signup' className='header__link'>
-          Регистрация
-        </NavLink>
-        <NavLink to='/signin' className='header__link header__link_main'>
-          Войти
-        </NavLink>
-      </div>
+      <nav className='header__nav'>
+        <ul className='header__nav-list'>
+          <li className='header__nav-item'>
+            <NavLink to='/signup' className='header__link'>
+              Регистрация
+            </NavLink>
+          </li>
+          <li className='header__nav-item'>
+            <NavLink to='/signin' className='header__link header__link_main'>
+              Войти
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -4,7 +4,7 @@ import './promo.css';
 function Promo() {
   return (
     <section className='promo'>
-      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      <p className='promo__title'>Учебный проект студента факультета Веб-разработки.</p>
     </section>
   );
 }
